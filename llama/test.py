a1463a37fbf4cb5b8753e85a9bedb306323dc2bd
muchichen <muchi674@gmail.com>
2024-02-22 20:10:05 +0400

    attempt to mimic mlx_llama func

--- llama/test.py
+++ llama/test.py
@@ -1,6 +1,46 @@
-import subprocess
+import argparse
+
+import mlx.core as mx
+from mlx_lm import load, generate
+
+
+def main(args):
+    # editted from:
+    # https://github.com/ml-explore/mlx-examples/blob/main/llms/mlx_lm/generate.py
+
+    default_seed = 0
+    default_temp = 0.6
+    mx.random.seed(default_seed)
+
+    model, tokenizer = load(args.model)
+
+    if (
+        hasattr(tokenizer, "apply_chat_template")
+        and tokenizer.chat_template is not None
+    ):
+        messages = [{"role": "user", "content": args.prompt}]
+        prompt = tokenizer.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+    else:
+        prompt = args.prompt
+
+    generate(model, tokenizer, args.prompt, temp=default_temp, verbose=True)
+
 
 if __name__ == "__main__":
-    subprocess.call(
-        'python -m mlx_lm.generate --model ~/Llama-2-7b-chat-hf --prompt "hello"'
+    parser = argparse.ArgumentParser(description="runs llama 2 with mlx_lm")
+    parser.add_argument(
+        "--model",
+        type=str,
+        help="path to self hf model stored locally"
+        + "see: https://huggingface.co/meta-llama",
     )
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        help="input to the specified model",
+    )
+    args = parser.parse_args()
+
+    main(args)
