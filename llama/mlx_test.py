4ac9126ddf7d53d138471c86fffa5180e1cb26c3
Mu-Chi Chen <muchi674@gmail.com>
2024-03-02 16:09:17 +0400

    add repr workload test

--- llama/mlx_test.py
+++ llama/mlx_test.py
@@ -1,26 +1,108 @@
+#!/Users/xiangruike/miniconda3/envs/custom_mlx_lm/bin/python
+
+import argparse
+import time
+
 import mlx.core as mx
 
 
-def simple_computations(a, b, d1, d2):
-    # copied from:
-    # https://ml-explore.github.io/mlx/build/html/usage/unified_memory.html
+class RepresentativeWorkload:
 
-    # compute dense, more suitable for GPU
-    # dimension: 4096 * 4
-    # 4096 * 4 / 1024 = 16 KiB
-    x = mx.matmul(a, b, stream=d1)
-    mx.eval(x)
-    for _ in range(500):
-        # less dense, faster on CPU
-        b = mx.exp(b, stream=d2)
-    mx.eval(b)
+    DEFAULT_INPUT_DIM = 32768
+
+    # mx.random.uniform() is used to produce the weights, of which
+    # the default return dtype is float32. Therefore, the size of
+    # each weights matrix is:
+    # (32768 * 32768 * 4) / (1024 * 1024 * 1024) = 4 GiB
+    DEFAULT_WEIGHTS_M = 32768
+    DEFAULT_WEIGHTS_N = 32768
+    DEFAULT_WEIGHTS_SIZE = 4  # in GiB
+
+    def __init__(self, total_weights_size: int, load_by_layer: bool) -> None:
+        """Encapsulates a simple yet memory demanding workload
+
+        Args:
+            total_weights_size (int): size of weights in GiB to load into unified memory.
+        """
+        assert total_weights_size % self.DEFAULT_WEIGHTS_SIZE == 0
+
+        mx.set_default_device(mx.gpu)
+        self.weights = self.load_weights(total_weights_size, load_by_layer)
+
+    def load_weights(self, total_weights_size: int, load_by_layer: bool):
+        num_layers = int(total_weights_size / self.DEFAULT_WEIGHTS_SIZE)
+        if load_by_layer:
+            weights = []
+            for _ in range(num_layers):
+                weights.append(
+                    mx.random.uniform(
+                        shape=(
+                            self.DEFAULT_WEIGHTS_M,
+                            self.DEFAULT_WEIGHTS_N,
+                        )
+                    )
+                )
+        else:
+            weights = mx.random.uniform(
+                shape=(
+                    num_layers,
+                    self.DEFAULT_WEIGHTS_M,
+                    self.DEFAULT_WEIGHTS_N,
+                )
+            )
+        mx.eval(weights)
+        return weights
+
+    def generate_token(self, input: mx.array) -> None:
+        token = input
+        for layer in self.weights:
+            token = mx.maximum(mx.matmul(token, layer), 0)
+        mx.eval(token)
+
+    def generate(self, n_tokens: int) -> None:
+        print("Generation Started", flush=True)
+        print("=" * 10, flush=True)
+
+        total_latency = 0
+        for i in range(n_tokens):
+            tic = time.perf_counter()
+
+            input = mx.random.uniform(shape=(self.DEFAULT_INPUT_DIM,))
+            self.generate_token(input)
+
+            toc = time.perf_counter()
+            latency = toc - tic
+            total_latency += latency
+            print(f"#{i} token generated in {latency} seconds", flush=True)
+
+        print("=" * 10)
+        print(f"AVERAGE TOKEN GENERATION SPEED: {n_tokens / total_latency} seconds")
+        print(f"TOTAL LATENCY: {total_latency} seconds")
 
 
 if __name__ == "__main__":
-    # default return dtype of mx.random.uniform is float32
-    # (4096 * 512) / (1024 * 1024) = 2 GiB
-    a = mx.random.uniform(shape=(4096, 512))
-    # (512 * 4) / 1024 = 2 KiB
-    b = mx.random.uniform(shape=(512, 4))
+    parser = argparse.ArgumentParser(description="runs llama 2 with mlx_lm")
+    parser.add_argument(
+        "--total-weights-size",
+        "-t",
+        type=int,
+        help="total weights size in GiB",
+    )
+    parser.add_argument(
+        "--n-tokens",
+        "-n",
+        type=int,
+        help="number of tokens to generate",
+    )
+    parser.add_argument(
+        "--load-by-layer",
+        "-l",
+        action="store_true",
+        help="whether to load each layer's weights separately",
+    )
+    args = parser.parse_args()
 
-    simple_computations(a, b, mx.gpu, mx.cpu)
+    model = RepresentativeWorkload(
+        total_weights_size=args.total_weights_size, load_by_layer=args.load_by_layer
+    )
+    model.generate(args.n_tokens)
