8bac9b32bffa959c2db02574dedb33c3ef6085b6
Mu-Chi Chen <muchi674@gmail.com>
2024-03-05 19:31:04 +0400

    allow configurable memory block size and num matmuls on test

--- llama/mlx_test.py
+++ llama/mlx_test.py
@@ -8,83 +8,96 @@ import mlx.core as mx
 
 class RepresentativeWorkload:
 
-    DEFAULT_INPUT_DIM = 16384
-
     # mx.random.uniform() is used to produce the weights, of which
     # the default return dtype is float32. Therefore, the size of
     # each weights matrix is:
-    # (16384 * 16384 * 4) / (1024 * 1024 * 1024) = 1 GiB
-    DEFAULT_WEIGHTS_M = 16384
-    DEFAULT_WEIGHTS_N = 16384
-    DEFAULT_WEIGHTS_SIZE = 1  # in GiB
-    # number of weight matrices per layer
-    # intends to mimic the multi-head attention mechanism which consists of
-    # query, key, value, and output projections
-    NUM_MAT_PER_LAYER = 4
+    # (32768 * 32768 * 4) / (1024 * 1024 * 1024) = 4 GiB
+    WEIGHTS_M = 32768
+    WEIGHTS_N = 32768
+    WEIGHTS_SIZE = 4  # in GiB
 
-    def __init__(self, total_weights_size: int, load_by_layer: bool) -> None:
+    def __init__(
+        self,
+        total_weights_size: int,
+        n_load_blocks: int,
+        n_matmuls_per_layer: int,
+        n_tokens: int,
+    ) -> None:
         """Encapsulates a simple yet memory demanding workload
 
         Args:
             total_weights_size (int): size of weights in GiB to load into unified memory.
         """
-        self.layer_weights_size = self.DEFAULT_WEIGHTS_SIZE * self.NUM_MAT_PER_LAYER
-        assert total_weights_size % self.layer_weights_size == 0
-        self.weights = self.load_weights(total_weights_size, load_by_layer)
+        self.total_weights_size = total_weights_size
+        self.n_load_blocks = n_load_blocks
+        self.n_matmuls_per_layer = n_matmuls_per_layer
+        self.n_tokens = n_tokens
+        self.num_layers = int(self.total_weights_size / self.WEIGHTS_SIZE)
 
-    def load_weights(self, total_weights_size: int, load_by_layer: bool):
-        num_layers = int(total_weights_size / self.layer_weights_size)
-        if load_by_layer:
+        assert self.total_weights_size % self.n_load_blocks == 0
+        self.weights_block_size = int(total_weights_size / n_load_blocks)
+
+        assert self.weights_block_size % self.WEIGHTS_SIZE == 0
+        self.n_weights_per_block = int(self.weights_block_size / self.WEIGHTS_SIZE)
+
+        self.weights = self.load_weights(n_load_blocks)
+
+    def load_weights(self):
+        if self.n_load_blocks > 1:
             print(
                 "\n------ "
-                + f"loading {num_layers} layers' weights individually "
-                + f"{self.layer_weights_size} GiB at a time "
+                + f"loading {self.n_load_blocks} blocks of weights sequentially as individual arrays "
+                + f"{self.weights_block_size} GiB at a time "
                 + "------",
                 flush=True,
             )
-            weights = []
-            for _ in range(num_layers):
-                weights.append(
-                    mx.random.uniform(
-                        shape=(
-                            self.NUM_MAT_PER_LAYER,
-                            self.DEFAULT_WEIGHTS_M,
-                            self.DEFAULT_WEIGHTS_N,
-                        )
+
+        weights = []
+
+        for _ in range(self.n_load_blocks):
+            weights.append(
+                mx.random.uniform(
+                    shape=(
+                        self.n_weights_per_block,
+                        self.WEIGHTS_M,
+                        self.WEIGHTS_N,
                     )
                 )
-        else:
-            weights = mx.random.uniform(
-                shape=(
-                    num_layers,
-                    self.NUM_MAT_PER_LAYER,
-                    self.DEFAULT_WEIGHTS_M,
-                    self.DEFAULT_WEIGHTS_N,
-                )
             )
+
+        # mlx has lazy evaluation: forcing load to happen here
         mx.eval(weights)
+
         return weights
 
-    def generate_token(self, input: mx.array) -> None:
-        token = input
-        for layer in self.weights:
-            # intends to mimic q, k, v, o projections
-            # see: https://github.com/ml-explore/mlx-examples/blob/main/llms/mlx_lm/models/llama.py
-            for proj in layer:
-                token = mx.matmul(token, proj)
-            token = mx.maximum(token, 0)
+    def generate_token(self) -> None:
+        token = None
+        for i in range(self.num_layers):
+            offset = i // self.n_weights_per_block
+            number = i % self.n_weights_per_block
+            layer_weights = self.weights[offset][number]
+            remaining_matmuls = self.n_matmuls_per_layer
+
+            if token is None:
+                # 1. only for the first time
+                # 2. eliminates the need for an input
+                # 3. defines a configurable unit of computation per layer: matmul
+                token = mx.matmul(layer_weights, layer_weights)
+                remaining_matmuls -= 1
+
+            for _ in range(remaining_matmuls):
+                mx.matmul(token, layer_weights)
         mx.eval(token)
 
-    def generate(self, n_tokens: int) -> None:
+    def generate(self) -> None:
         print("Generation Started", flush=True)
         print("=" * 10, flush=True)
 
         total_latency = 0
-        for i in range(n_tokens):
+        for i in range(self.n_tokens):
             tic = time.perf_counter()
 
-            input = mx.random.uniform(shape=(self.DEFAULT_INPUT_DIM,))
-            self.generate_token(input)
+            self.generate_token()
 
             toc = time.perf_counter()
             latency = toc - tic
@@ -92,7 +105,7 @@ class RepresentativeWorkload:
             print(f"#{i} token generated in {latency} seconds", flush=True)
 
         print("=" * 10)
-        print(f"AVERAGE TOKEN GENERATION SPEED: {n_tokens / total_latency} seconds")
+        print(f"AVG t/s: {self.n_tokens / total_latency}")
         print(f"TOTAL LATENCY: {total_latency} seconds")
 
 
@@ -100,9 +113,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="runs llama 2 with mlx_lm")
     parser.add_argument(
         "--total-weights-size",
-        "-t",
+        "-w",
         type=int,
         help="total weights size in GiB",
+    )
+    parser.add_argument(
+        "--n-load-blocks",
+        "-b",
+        type=int,
+        default=1,
+        help="load weights in the specified number separate blocks",
+    )
+    parser.add_argument(
+        "--matmuls-per-layer",
+        "-m",
+        type=int,
+        default=5,  # Q, K, V, Q * K_T, prev_results * V
+        help="number of matrix multiplications to perform per layer",
     )
     parser.add_argument(
         "--n-tokens",
@@ -110,16 +137,13 @@ if __name__ == "__main__":
         type=int,
         help="number of tokens to generate",
     )
-    parser.add_argument(
-        "--load-by-layer",
-        "-l",
-        action="store_true",
-        help="whether to load each layer's weights separately",
-    )
     args = parser.parse_args()
 
     mx.set_default_device(mx.gpu)
     model = RepresentativeWorkload(
-        total_weights_size=args.total_weights_size, load_by_layer=args.load_by_layer
+        args.total_weights_size,
+        args.n_load_blocks,
+        args.matmuls_per_layer,
+        args.n_tokens,
     )
-    model.generate(args.n_tokens)
+    model.generate()
