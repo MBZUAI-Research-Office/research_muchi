62c010ef9fd8562a740fd9ec57105bba7a99cb09
Mu-Chi Chen <muchi674@gmail.com>
2024-04-14 00:11:09 +0400

    i want to sleep

--- dbrx/driver.py
+++ dbrx/driver.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from typing import Optional, Tuple
 import argparse
 import asyncio
+import json
 import logging
 import time
 
@@ -18,24 +19,6 @@ import mlx.core as mx
 import mlx.nn as nn
 
 from base import BaseModelArgs
-
-NUM_LAYERS = 40
-EMBEDDING_LENGTH = 6144
-NUM_EXPERTS = 4
-TOP_K = 4
-DUMMY_NP_DATA = np.random.uniform(-1, 1, EMBEDDING_LENGTH).astype(np.float32)
-# EXPERT_CHANNELS = [
-#     "169.254.238.2:2000",
-#     "169.254.238.4:4000",
-#     "169.254.238.5:5000",
-#     "169.254.238.6:6000",
-# ]
-EXPERT_CHANNELS = [
-    # "169.254.136.2:2000",
-    # "169.254.136.4:4000",
-    # "169.254.136.5:5000",
-    "169.254.136.6:6000",
-]
 
 
 @dataclass
@@ -138,21 +121,36 @@ class Router(nn.Module):
         return self.layer(x)
 
 
-# TODO: this needs to change
 class DistributedSparseMoeBlock(nn.Module):
     def __init__(self, args: ModelArgs):
         super().__init__()
-        self.d_model = args.d_model
-        self.ffn_dim = args.ffn_config["ffn_hidden_size"]
-        self.num_experts = args.ffn_config["moe_num_experts"]
         self.num_experts_per_tok = args.ffn_config["moe_top_k"]
 
-        self.router = Router(self.d_model, self.num_experts)
-        self.experts = [
-            MLP(self.d_model, self.ffn_dim) for _ in range(self.num_experts)
-        ]
+        # shards are moe_shard_pb2_grpc.MoeShardStub and thus cannot be dict key
+        self.expert_to_shard = args.ffn_config["expert_to_shard_num"]
+        self.shards = args.moe_shards
 
-    def __call__(self, x: mx.array) -> mx.array:
+        self.router = Router(args.d_model, args.ffn_config["moe_num_experts"])
+
+    async def execute_on_shard(
+        self,
+        shard: moe_shard_pb2_grpc.MoeShardStub,
+        block_num: int,
+        activated_experts: list,
+        x: mx.array,
+    ):
+        outputs = await shard.Execute(
+            moe_shard_pb2.Inputs(
+                block_num=block_num,
+                activated_experts=np.array(activated_experts).tobytes(),
+                data=np.array(x.astype(mx.float32)).tobytes(),
+            )
+        )
+        return mx.array(
+            np.frombuffer(outputs.data, dtype=np.float32), dtype=mx.bfloat16
+        )
+
+    async def __call__(self, x: mx.array, block_num: int) -> mx.array:
         ne = self.num_experts_per_tok
         orig_shape = x.shape
         x = x.reshape(-1, x.shape[-1])
@@ -167,7 +165,21 @@ class DistributedSparseMoeBlock(nn.Module):
 
         y = []
         for xt, st, it in zip(x, scores, inds.tolist()):
-            yt = mx.stack([self.experts[e](xt) for e in it], axis=-1)
+            shard_to_experts = {}
+            for e in it:
+                shard_to_experts.setdefault(self.expert_to_shard[e], []).append(e)
+
+            async with asyncio.TaskGroup() as tg:
+                yt = [
+                    tg.create_task(
+                        self.execute_on_shard(
+                            self.shards[si], block_num, activated_experts, xt
+                        )
+                    )
+                    for si, activated_experts in shard_to_experts.items()
+                ]
+
+            yt = mx.stack(mx.concatenate(yt, axis=0), axis=-1)
             yt = (yt * st).sum(axis=-1)
             y.append(yt)
         y = mx.stack(y, axis=0)
@@ -176,10 +188,11 @@ class DistributedSparseMoeBlock(nn.Module):
 
 
 class DistributedDecoderLayer(nn.Module):
-    def __init__(self, args: ModelArgs):
+    def __init__(self, args: ModelArgs, block_num: int):
         super().__init__()
         self.ffn = DistributedSparseMoeBlock(args)
         self.norm_attn_norm = NormAttnNorm(args)
+        self.block_num = block_num
 
     def __call__(
         self,
@@ -188,7 +201,7 @@ class DistributedDecoderLayer(nn.Module):
         cache: Optional[Tuple[mx.array, mx.array]] = None,
     ) -> mx.array:
         r, h, cache = self.norm_attn_norm(x, mask, cache)
-        out = self.ffn(h) + r
+        out = self.ffn(h, self.block_num) + r
         return out, cache
 
 
@@ -197,7 +210,7 @@ class DistributedDBRX(nn.Module):
         super().__init__()
         self.vocab_size = args.vocab_size
         self.wte = nn.Embedding(args.vocab_size, args.d_model)
-        self.blocks = [DistributedDecoderLayer(args) for _ in range(args.n_layers)]
+        self.blocks = [DistributedDecoderLayer(args, i) for i in range(args.n_layers)]
         self.norm_f = nn.LayerNorm(args.d_model, bias=False)
         self.lm_head = nn.Linear(args.d_model, args.vocab_size, bias=False)
 
@@ -223,17 +236,196 @@ class DistributedDBRX(nn.Module):
         return self.lm_head(self.norm_f(h)), cache
 
 
-async def execute_on_expert(stub, block_num, activated_experts, data, outputs):
-    out = await stub.Execute(
-        moe_shard_pb2.Inputs(
-            block_num=block_num,
-            activated_experts=activated_experts.tobytes(),
-            data=data.tobytes(),
-        )
-    )
-    outputs.append(np.frombuffer(out.data, dtype=np.float32))
-    return
+def load_model(self) -> nn.Module:
+    # shards = [moe_shard_pb2_grpc.MoeShardStub(channel) for channel in channels]
+    # {
+    #     0: shard[0],
+    #     1: shard[0],
+    #     ...
+    #     4: shard[1]
+    # }
+    try:
+        with open(self.model_path / "driver_config.json", "r") as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        logging.error(f"shard_config.json not found in {self.model_path}")
+        raise
 
+    model_args = ModelArgs.from_dict(config)
+    model = DistributedDBRX(model_args)
+
+    weights = {}
+    for i in model_args.ffn_config["assigned_experts"]:
+        weights.update(mx.load(str(self.model_path / f"expert{i}.npz")))
+
+    model.load_weights(list(weights.items()))
+    mx.eval(model.parameters())
+    model.eval()
+
+    return model
+
+def generate_step(
+    prompt: mx.array,
+    model: nn.Module,
+    temp: float = 0.0,
+    repetition_penalty: Optional[float] = None,
+    repetition_context_size: Optional[int] = 20,
+    top_p: float = 1.0,
+) -> Generator[Tuple[mx.array, mx.array], None, None]:
+    """
+    A generator producing text based on the given prompt from the model.
+
+    Args:
+        prompt (mx.array): The input prompt.
+        model (nn.Module): The model to use for generation.
+        temp (float): The temperature for sampling, if 0 the argmax is used.
+        repetition_penalty (float, optional): The penalty factor for repeating tokens.
+        repetition_context_size (int, optional): The number of tokens to consider for repetition penalty (default 20).
+        top_p (float, optional): Nulceus sampling, higher means model considers more less likely words
+
+    Yields:
+        Generator[Tuple[mx.array, mx.array]]: A generator producing
+        one token and probability per call.
+    """
+
+    def sample(logits: mx.array) -> Tuple[mx.array, float]:
+        softmax_logits = mx.softmax(logits)
+
+        if temp == 0:
+            token = mx.argmax(logits, axis=-1)
+        else:
+            if top_p > 0 and top_p < 1.0:
+                token = top_p_sampling(logits, top_p, temp)
+            else:
+                token = mx.random.categorical(logits * (1 / temp))
+
+        prob = softmax_logits[0, token]
+        return token, prob
+
+    if repetition_penalty and (
+        repetition_penalty < 0 or not isinstance(repetition_penalty, float)
+    ):
+        raise ValueError(
+            f"repetition_penalty must be a non-negative float, got {repetition_penalty}"
+        )
+
+    y = prompt
+    cache = None
+
+    repetition_context = prompt.tolist()
+
+    if repetition_context_size:
+        repetition_context = repetition_context[-repetition_context_size:]
+
+    while True:
+        logits, cache = model(y[None], cache=cache)
+        logits = logits[:, -1, :]
+
+        if repetition_penalty:
+            logits = apply_repetition_penalty(
+                logits, repetition_context, repetition_penalty
+            )
+            y, prob = sample(logits)
+            repetition_context.append(y.item())
+        else:
+            y, prob = sample(logits)
+
+        if repetition_context_size:
+            if len(repetition_context) > repetition_context_size:
+                repetition_context = repetition_context[-repetition_context_size:]
+        yield y, prob
+
+
+def generate(
+    model: nn.Module,
+    tokenizer: PreTrainedTokenizer,
+    prompt: str,
+    temp: float = 0.0,
+    max_tokens: int = 100,
+    verbose: bool = False,
+    formatter: Optional[Callable] = None,
+    repetition_penalty: Optional[float] = None,
+    repetition_context_size: Optional[int] = None,
+    top_p: float = 1.0,
+) -> str:
+    """
+    Generate text from the model.
+
+    Args:
+       model (nn.Module): The language model.
+       tokenizer (PreTrainedTokenizer): The tokenizer.
+       prompt (str): The string prompt.
+       temp (float): The temperature for sampling (default 0).
+       max_tokens (int): The maximum number of tokens (default 100).
+       verbose (bool): If ``True``, print tokens and timing information
+           (default ``False``).
+       formatter (Optional[Callable]): A function which takes a token and a
+           probability and displays it.
+       repetition_penalty (float, optional): The penalty factor for repeating tokens.
+       repetition_context_size (int, optional): The number of tokens to consider for repetition penalty.
+    """
+
+    if verbose:
+        print("=" * 10)
+        print("Prompt:", prompt)
+
+    prompt_tokens = mx.array(tokenizer.encode(prompt))
+
+    tic = time.perf_counter()
+    tokens = []
+    token_strings = []
+    skip = 0
+    REPLACEMENT_CHAR = "\ufffd"
+
+    for (token, prob), n in zip(
+        generate_step(
+            prompt_tokens,
+            model,
+            temp,
+            repetition_penalty,
+            repetition_context_size,
+            top_p,
+        ),
+        range(max_tokens),
+    ):
+        token = token.item()
+        if n == 0:
+            prompt_time = time.perf_counter() - tic
+            tic = time.perf_counter()
+        if token == tokenizer.eos_token_id:
+            break
+        tokens.append(token)
+
+        if verbose:
+            s = tokenizer.decode(tokens)
+            if formatter:
+                formatter(s[skip:], prob.item())
+                skip = len(s)
+            elif s[-1] != REPLACEMENT_CHAR:
+                print(s[skip:], end="", flush=True)
+                skip = len(s)
+            # Reset token cache at line break
+            if s[-1] == "\n":
+                tokens = []
+                token_strings.append(s)
+                skip = 0
+
+    token_count = n + 1
+    token_strings.append(tokenizer.decode(tokens).replace(REPLACEMENT_CHAR, ""))
+
+    if verbose:
+        print(token_strings[-1][skip:], flush=True)
+        gen_time = time.perf_counter() - tic
+        print("=" * 10)
+        if token_count == 0:
+            print("No tokens generated for this prompt")
+            return
+        prompt_tps = prompt_tokens.size / prompt_time
+        gen_tps = (token_count - 1) / gen_time
+        print(f"Prompt: {prompt_tps:.3f} tokens-per-sec")
+        print(f"Generation: {gen_tps:.3f} tokens-per-sec")
+
+    return "".join(token_strings)
 
 async def generate(num_tokens: int):
     async with AsyncExitStack() as es:
