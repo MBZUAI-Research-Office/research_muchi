1a07fad015e354fca1960b0f367436615daa4179
Mu-Chi Chen <muchi674@gmail.com>
2024-05-29 17:19:19 +0400

    [DEBUG] v4 comm overlap test: make envoy synchronous

--- dbrx/v4_streaming/comm_overlap_test/shard.py
+++ dbrx/v4_streaming/comm_overlap_test/shard.py
@@ -1,12 +1,11 @@
 #!/Users/xiangruike/miniconda3/envs/dbrx_poc/bin/python
 
-from contextlib import AsyncExitStack
+from contextlib import ExitStack
+from concurrent import futures
 from multiprocessing import connection
 from pathlib import Path
 from typing import Any
 import argparse
-import asyncio
-import concurrent.futures
 import json
 import logging
 import pickle
@@ -44,7 +43,7 @@ def shard_main(conn: connection.Connection) -> None:
     logging.basicConfig(level=logging.INFO)
     generator = Generator(conn)
     logging.info("generator ready")
-    batch_size = conn.recv() # confirm that everyone is good to go
+    batch_size = conn.recv()  # confirm that everyone is good to go
     generator.start(batch_size)
 
 
@@ -54,11 +53,9 @@ class Buffer:
         self.n_layers = n_layers
         self.bin_size = n_oth_shards
         self.data = None
-        self.is_full_events = None
 
     def set_up(self, batch_size: int, for_warming: bool = False) -> None:
         self.data = []
-        self.is_full_events = []
 
         n_bins = batch_size * self.n_layers
         if for_warming:
@@ -66,18 +63,14 @@ class Buffer:
 
         for _ in range(n_bins):
             self.data.append([])
-            self.is_full_events.append(asyncio.Event())
 
     def put(self, d: Any, bin_num: int) -> None:
         self.data[bin_num].append(d)
-        if len(self.data[bin_num]) == self.bin_size:
-            self.is_full_events[bin_num].set()
 
-    def get_data(self, bin_num: int) -> Any:
+    def get(self, bin_num: int) -> Any:
+        while len(self.data[bin_num]) < self.bin_size:
+            pass
         return self.data[bin_num]
-
-    def get_is_full_signal(self, bin_num: int) -> asyncio.Event:
-        return self.is_full_events[bin_num]
 
 
 class ShardEnvoyServicer(shard_envoy_pb2_grpc.ShardEnvoyServicer):
@@ -110,9 +103,7 @@ class ShardEnvoyServicer(shard_envoy_pb2_grpc.ShardEnvoyServicer):
 
         return config
 
-    def broadcast_im_ready(
-        self, executor: concurrent.futures.Executor, oth_shards: dict
-    ):
+    def broadcast_im_ready(self, executor: futures.Executor, oth_shards: dict):
         def signal(shard):
             shard.SignalReady(shard_envoy_pb2.Empty())
 
@@ -126,7 +117,7 @@ class ShardEnvoyServicer(shard_envoy_pb2_grpc.ShardEnvoyServicer):
         self,
         bi: int,
         li: int,
-        executor: concurrent.futures.Executor,
+        executor: futures.Executor,
         oth_shards: dict,
     ) -> None:
 
@@ -158,12 +149,12 @@ class ShardEnvoyServicer(shard_envoy_pb2_grpc.ShardEnvoyServicer):
         logging.info(f"finished receiving")
         return shard_envoy_pb2.Empty()
 
-    async def Start(self, request: shard_envoy_pb2.TestIns, context):
+    def Start(self, request: shard_envoy_pb2.TestIns, context):
         self.buffer.set_up(request.batch_size, for_warming=True)
-        async with AsyncExitStack() as es:
+        with ExitStack() as es:
             oth_shards = {}
             for url in self.config["oth_urls"]:
-                channel = await es.enter_async_context(
+                channel = es.enter_context(
                     grpc.aio.insecure_channel(
                         url,
                         options=[
@@ -175,56 +166,35 @@ class ShardEnvoyServicer(shard_envoy_pb2_grpc.ShardEnvoyServicer):
                 shard = shard_envoy_pb2_grpc.ShardEnvoyStub(channel)
                 oth_shards[url] = shard
 
-            with concurrent.futures.ThreadPoolExecutor() as executor:
+            with futures.ThreadPoolExecutor() as executor:
 
-                concurrent.futures.wait(self.broadcast_im_ready(executor, oth_shards))
-                await self.buffer.get_is_full_signal(-1).wait()
-                logging.info("everyone is ready")
-                self.conn.send(request.batch_size) # signal generator to start working
+                futures.wait(self.broadcast_im_ready(executor, oth_shards))
+                self.buffer.get(-1) # block until everyone is ready
+                self.conn.send(request.batch_size)  # signal generator to start working
 
                 for bi in range(request.batch_size):
                     self.all_dispatch(bi, 0, executor, oth_shards)
 
 
-async def serve(
-    port: int, model_path: str, config_filename: str, conn: connection.Connection
-):
-    server = grpc.aio.server(
-        options=[
-            ("grpc.max_send_message_length", -1),
-            ("grpc.max_receive_message_length", -1),
-        ]
-    )
-    servicer = ShardEnvoyServicer(model_path, config_filename, conn)
-    shard_envoy_pb2_grpc.add_ShardEnvoyServicer_to_server(servicer, server)
-    listen_addr = f"[::]:{port}"
-    server.add_insecure_port(listen_addr)
-    await server.start()
-    logging.info(f"server started, listening on {listen_addr}")
-
-    # copied from:
-    # https://github.com/grpc/grpc/blob/master/examples/python/helloworld/async_greeter_server_with_graceful_shutdown.py
-    async def server_graceful_shutdown():
-        logging.info("Starting graceful shutdown...")
-        # Shuts down the server with 3 seconds of grace period. During the
-        # grace period, the server won't accept new connections and allow
-        # existing RPCs to continue within the grace period.
-        await server.stop(3)
-
-    _cleanup_coroutines.append(server_graceful_shutdown())
-    await server.wait_for_termination()
-
-
-def envoy_main(
+async def envoy_main(
     port: int, model_path: str, config_filename: str, conn: connection.Connection
 ):
     logging.basicConfig(level=logging.INFO)
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(serve(port, model_path, config_filename, conn))
-    finally:
-        loop.run_until_complete(*_cleanup_coroutines)
-        loop.close()
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10),
+        options=[
+            ("grpc.max_send_message_length", -1),
+            ("grpc.max_receive_message_length", -1),
+        ],
+    )
+    shard_envoy_pb2_grpc.add_ShardEnvoyServicer_to_server(
+        ShardEnvoyServicer(model_path, config_filename, conn), server
+    )
+    listen_addr = f"[::]:{port}"
+    server.add_insecure_port(listen_addr)
+    server.start()
+    logging.info(f"server started, listening on {listen_addr}")
+    server.wait_for_termination()
 
 
 if __name__ == "__main__":
