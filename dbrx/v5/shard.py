da4ed7245f955ce28cdfa034b312ae37e72bfe23
Mu-Chi Chen <muchi674@gmail.com>
2024-06-13 00:35:17 +0400

    hahaha

--- dbrx/v5/shard.py
+++ dbrx/v5/shard.py
@@ -315,7 +315,7 @@ class DistributedMoeBlock(nn.Module):
     ) -> dict:
         shard_outs = {}
         for bi, xt in enumerate(x):
-            time.sleep(0.0001)
+            time.sleep(0.01)
             expert_outs, arr_map = shard(xt, jobs[bi], bool(bi > 0))
             shard_outs.setdefault(self.url, {})[bi] = (expert_outs, arr_map)
             send_conn.send_bytes(mx_to_bytes(expert_outs))
