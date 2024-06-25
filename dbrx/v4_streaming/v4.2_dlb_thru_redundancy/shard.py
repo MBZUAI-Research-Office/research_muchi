d00aec33ef3944304b3865e2ac86c65971078e12
Mu-Chi Chen <muchi674@gmail.com>
2024-06-25 16:54:43 +0400

    [EXPERIMENT] change distributed MoE evaluation strategy

--- dbrx/v4_streaming/v4.2_dlb_thru_redundancy/shard.py
+++ dbrx/v4_streaming/v4.2_dlb_thru_redundancy/shard.py
@@ -216,10 +216,8 @@ class MoeShard:
                 mlp(x, v1, w1, w2, expert_outs)
                 arr_map[e] = len(expert_outs) - 1
 
-        expert_outs = mx.stack(expert_outs, axis=0)
         mx.eval(expert_outs)
-
-        return expert_outs, arr_map
+        return mx.stack(expert_outs, axis=0), arr_map
 
 
 class DistributedMoeBlock(nn.Module):
