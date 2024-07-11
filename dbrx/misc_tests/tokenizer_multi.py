b27b2027d04c9b05f044c30d2e6d5d869546a237
Mu-Chi Chen <muchi674@gmail.com>
2024-07-11 09:43:44 +0400

    [EXPERIMENT] tokenize multi prompts

---
+++ dbrx/misc_tests/tokenizer_multi.py
@@ -0,0 +1,23 @@
+#!/Users/xiangruike/miniconda3/envs/dbrx_poc/bin/python
+
+from pathlib import Path
+
+import mlx.core as mx
+
+from transformers import AutoTokenizer
+
+
+class Test:
+
+    def __init__(self, model_path: str) -> None:
+        self.model_path = Path(model_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_path, trust_remote_code=True
+        )
+
+    def test(self, prompts: list[str]):
+        print(self.tokenizer(prompts))
+
+if __name__ == "__main__":
+    test = Test("/Users/xiangruike/dbrx-instruct/distributable/batch2")
+    test.test(["hello there", "how are you?"])
